221ea752d3238ec51aa1aefe7ed21cbc29963295
xcitic <xcitic@gmail.com>
2019-08-21 21:05:41 +0200

    Controller & Methods Post

---
+++ lumen/app/Http/Controllers/PostController.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+
+  /**
+   * Get all Posts
+   * @return Array
+   */
+  public function index() {
+    $posts = Post::all();
+
+    return response()->json($posts, 200);
+  }
+
+  /**
+   * Create a new Post
+   * @param  Request $request [title, content]
+   * @return Object
+   */
+  public function create(Request $request) {
+    $post = new Post();
+
+    $post->title = $request->title;
+    $post->content = $request->content;
+    $post->save();
+
+    return response()->json($post, 201);
+  }
+
+
+  /**
+   * Return a single Post
+   * @param  int    $id
+   * @return Object
+   */
+  public function show(int $id) {
+    $post = Post::find($id);
+
+    return response()->json($post, 200);
+  }
+
+  /**
+   * Update a Post
+   * @param  Request $request [title, content]
+   * @param  int  $id
+   * @return Object
+   */
+  public function update(Request $request, int $id) {
+    $post = Post::find($id);
+
+    $post->title = $request->title;
+    $post->content = $request->content;
+
+    $post->update();
+
+    return response()->json($post, 200);
+  }
+
+
+  /**
+   * Delete a Post
+   * @param  int    $id
+   * @return String  
+   */
+  public function destroy(int $id) {
+    $post = Post::find($id);
+    $post->delete();
+
+    return response()->json('Post removed', 200);
+  }
+
+}
--- lumen/app/Post.php
+++ lumen/app/Post.php
@@ -4,7 +4,7 @@ namespace App;
 
 
 class Post {
-  
+
   protected $table = 'products';
 
   protected $fillable = [
