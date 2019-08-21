981ed1a2c528cdf551e4b8008780cf041c34fa2f
xcitic <xcitic@gmail.com>
2019-08-21 21:09:24 +0200

    Routes

--- lumen/routes/web.php
+++ lumen/routes/web.php
@@ -14,3 +14,12 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+
+$app->group(['prefix' => 'api/v1'], function() use ($app){
+  $app->get('/posts', 'PostController@index');
+  $app->get('/post', 'PostController@create');
+  $app->get('/post/{id}', 'PostController@show');
+  $app->put('/post/{id}', 'PostController@update');
+  $app->delete('/post/{id}', 'PostController@destroy');
+});
